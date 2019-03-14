795cf312fae11b0abc258bb8b9802a97d4af85ce
John Sutray <JohnSutray@gmail.com>
2019-03-14 21:51:47 +0300

    Initial commit

---
+++ Task4(Olya)/src/task4/MatrixCalculator.java
@@ -0,0 +1,40 @@
+package task4;
+
+public class MatrixCalculator {
+    public static void swapCols(double[][] array, int first, int second) {
+        double temp;
+        double firstValue;
+        double secondValue;
+        for (double[] line: array) {
+            temp = line[first];
+            line[first] = line[second];
+            line[second] = temp;
+        }
+    }
+
+    public static double getColAbsSum(double[][] array, int colIndex) {
+        double sum = 0;
+        for (double[] line: array) {
+            sum += line[colIndex];
+        }
+        return sum;
+    }
+
+    public static void sortMatrix(double[][] array) {
+        boolean action = false;
+        for (int j = 0; j < array.length; j++) {
+            for (int i = 0; i < array.length; i++) {
+                if (i < array.length - 1 && getColAbsSum(array, i) < getColAbsSum(array, i + 1)) {
+                    swapCols(array, i, i + 1);
+                    action = true;
+                }
+            }
+            if (!action) {
+                return;
+            }
+            else {
+                action = false;
+            }
+        }
+    }
+}
---
+++ Task4(Olya)/src/task4/tests/MatrixCalculatorTest.java
@@ -0,0 +1,55 @@
+package task4.tests;
+
+import task4.MatrixCalculator;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MatrixCalculatorTest {
+
+    @Test
+    public void canSwapCols() {
+        double[][] array = {
+                {0, 1},
+                {0, 1}
+        };
+
+        double[][] expected = {
+                {1, 0},
+                {1, 0}
+        };
+
+        MatrixCalculator.swapCols(array, 0, 1);
+
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void canGetColAbsSum() {
+        double[][] array = {
+                {1, 2},
+                {3, 4}
+        };
+
+        double expected = 4;
+        double actual = MatrixCalculator.getColAbsSum(array, 0);
+
+        assertEquals(expected, actual, 0.001);
+    }
+
+    @Test
+    public void canSortMatrix() {
+        double[][] array = {
+                { 1, 2 },
+                { 1, 2 }
+        };
+
+        double[][] expected = {
+                { 2, 1 },
+                { 2, 1 }
+        };
+
+        MatrixCalculator.sortMatrix(array);
+
+        assertArrayEquals(expected, array);
+    }
+}
